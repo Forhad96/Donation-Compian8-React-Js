a5ddb4fcdf17cadb55ddb7a6e2a8811855c96784
Forhad96 <forhadairdrop@gmail.com>
2023-09-25 23:40:46 +0600

    banner details button background opacity added

--- src/pages/DonationDetails/DonationDetails.jsx
+++ src/pages/DonationDetails/DonationDetails.jsx
@@ -3,32 +3,36 @@ import { Link, useLoaderData, useParams } from "react-router-dom";
 import { savaToLs } from "../../utility/localStorage";
 
 const DonationDetails = () => {
-    const donations = useLoaderData({});
-    const [details,setDetails] = useState({})
-    const {images,title,donation_amount,description} = details
+  const donations = useLoaderData({});
+  const [details, setDetails] = useState({});
+  const { images, title, donation_amount, description } = details;
 
-    const {id} = useParams()
-    const idInt = parseInt(id)
-    useEffect(()=>{
-        if(donations){
-            const filterDetails = donations.find(donation => donation.id === idInt)
-            setDetails(filterDetails)
-        }
-    },[donations,idInt])
-
-    const handleDonate =()=>{
-        savaToLs(idInt)
+  const { id } = useParams();
+  const idInt = parseInt(id);
+  useEffect(() => {
+    if (donations) {
+      const filterDetails = donations.find((donation) => donation.id === idInt);
+      setDetails(filterDetails);
     }
+  }, [donations, idInt]);
+
+  const handleDonate = () => {
+    savaToLs(idInt);
+  };
 
   return (
     <div>
-        <div>
+      <div>
         <img className="w-full" src={images} alt="" />
-        <Link to={`/donation`}>
-        <button onClick={handleDonate} className="btn bg-primary-color text-white text-xl font-semibold relative bottom-28 left-5">
-          Donate$ {donation_amount}
-        </button>
-        </Link>
+        <div className="bg-gray-400 bg-opacity-60 py-6 relative bottom-24">
+          <Link to={`/donation`}>
+            <button
+              onClick={handleDonate}
+              className="btn bg-primary-color text-white text-xl font-semibold relative left-5  ">
+              Donate$ {donation_amount}
+            </button>
+          </Link>
+        </div>
       </div>
       <h2 className="text-4xl font-bold">{title}</h2>
       <p>{description}</p>
